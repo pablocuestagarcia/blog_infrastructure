4b4f04e5934b030104c96fb8aaaf7d2931eca8bb
Pablo de la Cuesta <pablodelacuestagarcia@gmail.com>
2023-05-09 07:54:43 +0200

    init project

--- src/environments/development/versions.tf
+++ src/environments/development/versions.tf
@@ -2,12 +2,11 @@ terraform {
   required_providers {
     digitalocean = {
       source = "digitalocean/digitalocean"
-      version = "2.28.0"
+      version = "2.28.1"
     }
   }
 }
 
 provider "digitalocean" {
-  # Configuration options
   token = var.do_token
 }
